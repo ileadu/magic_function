df486221789b700d9622c3bbeb5015ca385c8178
ileadu <ileadu@gmail.com>
2024-01-19 10:53:01 +0800

    最初測試版

---
+++ Address.php
@@ -0,0 +1,124 @@
+<?php
+
+class Address
+{
+
+    public function __construct(
+        public string $street,
+        protected string $city,
+        private readonly string $state,
+        private ?int $zip = null,
+        private readonly string $type = 'address'
+    ) {
+        $this->connect();
+        $this->dump('Open connection');
+    }
+
+    public function __invoke(): void
+    {
+        $this->dump('Address object is called as a function!');
+    }
+
+    public function __get(string $name): mixed
+    {
+        $this->dump("Address `$name` property was retrieve using __get");
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
+        return null;
+    }
+
+    public function __set(string $name, mixed $value): void
+    {
+        $this->dump("Address `$name` property was __set to: `$value`");
+        if (property_exists($this, $name)) {
+            $this->$name = $value;
+        }
+    }
+
+    public function __isset(string $name): bool
+    {
+        $this->dump("Address `$name` property checked if __isset");
+        return isset($this->$name);
+    }
+
+    public function __unset(string $name): void
+    {
+        $this->dump("Address `$name` property was __unset");
+        $this->{$name} = null;
+    }
+
+    public function __toString(): string
+    {
+        $this->dump("Address properties were returned as string using __toString");
+        return "{$this->type}: {$this->street}, {$this->city}, {$this->state}, {$this->zip}.";
+    }
+
+    public function __clone(): void
+    {
+        $this->dump('Address object called __clone');
+        trigger_error('Cloning forbidden.', E_USER_WARNING);
+    }
+
+    public function __sleep(): array
+    {
+        $this->dump('Address attributes to serialize, called __sleep');
+        return array('street', 'zip');
+    }
+
+    public function __wakeup(): void
+    {
+        $this->dump('Reconnect to DB __wakeup');
+        $this->connect();
+    }
+
+    public function __destruct()
+    {
+        $this->dump('Close connection to DB and do other cleanups');
+        $this->closeConnection();
+    }
+
+    public function __call(string $name, mixed $arguments): mixed
+    {
+        $this->dump("Calling object method '$name' " . implode(', ', $arguments));
+        if ($name === 'format') {
+            return $this->displayAddress();
+        }
+    }
+
+    public static function __callStatic(string $name, mixed $arguments): ?Address
+    {
+        self::sdump("Calling static method '$name' " . implode(', ', $arguments));
+        if ($name === 'getDefaultAddress') {
+            return new Address('123 Main St', 'Meriland', 'US', 1000);
+        }
+    }
+
+    private function connect(): void
+    {
+        $this->dump('Connect to DB');
+    }
+
+    private function closeConnection(): void
+    {
+        $this->dump('Close connection to DB');
+    }
+
+    public function displayAddress(): string
+    {
+        return "{$this->street}, {$this->city}, {$this->state}";
+    }
+
+    private function dump(string $message): void
+    {
+        $functionName = debug_backtrace()[1]['function'];
+        echo "[$functionName] $message" . PHP_EOL;
+    }
+
+    private static function sdump(string $message): void
+    {
+        $functionName = debug_backtrace()[1]['function'];
+        echo "[$functionName] $message" . PHP_EOL;
+    }
+
+}
---
+++ test.php
@@ -0,0 +1,84 @@
+<?php
+require 'Address.php';
+
+/**
+ *  PHP Magic
+ *
+ * 了解 PHP 魔術方法的實際範例
+ * @see https://medium.com/@erlandmuchasaj/php-magic-0ae9e9b5442c
+ */
+
+// 步驟 1: 創建 Address 類的新實例
+$address = new Address('內湖區瑞光路', '台北市', '台灣', 114);
+
+var_dump($address);
+// 步驟 2: 使用魔術方法 __toString 將完整地址以字串形式輸出
+echo "Full address as string: {$address}" . PHP_EOL;
+echo PHP_EOL;
+// 步驟 3: 使用魔術方法 __set 更新街道屬性
+// No magic method called.
+$address->street = '三重區集美街';
+
+// 步驟 4: 使用魔術方法 __set 更新城市屬性
+// Access properties using __set since is protected
+$address->city = '新北市';
+
+// 步驟 5: 使用魔術方法 __get 輸出城市屬性
+// Access properties using __get
+echo "City: {$address->city} " . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 6: 使用魔術方法 __isset 檢查郵遞區號屬性是否設定
+// Check if a property is set using __isset
+echo 'check Zip if set: ' . (isset($address->zip) ? 'Y' : 'N') . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 7: 使用魔術方法 __toString 將完整地址以字串形式輸出
+// Convert the object to a string using __toString
+echo "Full address as string: {$address}" . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 8: 使用魔術方法 __unset 解除設定郵遞區號屬性
+// Unset a property using __unset
+unset($address->zip);
+echo 'check Zip if set: ' . (isset($address->zip) ? 'Y' : 'N') . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 9: 使用魔術方法 __sleep 對物件進行序列化
+// Set object properties using __sleep
+$serialized = serialize($address);
+echo "Object address serialized: $serialized" . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 10: 使用魔術方法 __call 呼叫 format 方法
+// Call a method using __call
+echo "Formatted address:" . $address->format() . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 11: 使用魔術方法 __callStatic 呼叫 getDefaultAddress 靜態方法
+// Call a static method using __callStatic
+$defaultAddress = Address::getDefaultAddress(1);
+echo "Default Address: {$defaultAddress}" . PHP_EOL;
+echo PHP_EOL;
+
+// 步驟 12: 使用魔術方法 __clone 複製物件
+// Call __clone method of object.
+$newAddress = clone $address;
+echo "Full newAddress after clone as string: {$newAddress}" . PHP_EOL;
+echo PHP_EOL;
+var_dump($address);
+var_dump($newAddress);
+// 步驟 13: 使用魔術方法 __invoke 呼叫物件本身作為函式
+$newAddress();
+var_dump($newAddress);
+// 步驟 3: 使用魔術方法 __set 更新街道屬性
+// No magic method called.
+$newAddress->street = '神岡區中山路';
+
+// 步驟 4: 使用魔術方法 __set 更新城市屬性
+// Access properties using __set since is protected
+$newAddress->city = '台中市';
+
+var_dump($newAddress);
+echo "Full newAddress after reset as string: {$newAddress}" . PHP_EOL;
+echo PHP_EOL;
